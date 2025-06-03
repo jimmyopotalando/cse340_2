faa1c8d4b852b91408cf33b818db9635a1694fef
jimmy opot alando <jimmyopot18@gmail.com>
2025-06-03 10:15:36 +0300

    W03 Assignment: Vehicle Detail Page

--- database/index.js
+++ database/index.js
@@ -1,23 +1,13 @@
-function buildVehicleDetailView(vehicle) {
-    const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(vehicle.inv_price);
-    const miles = new Intl.NumberFormat().format(vehicle.inv_miles);
-  
-    return `
-      <div class="vehicle-detail">
-        <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}" />
-        <div class="vehicle-info">
-          <h2>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h2>
-          <p><strong>Price:</strong> ${price}</p>
-          <p><strong>Mileage:</strong> ${miles} miles</p>
-          <p><strong>Description:</strong> ${vehicle.inv_description}</p>
-          <p><strong>Color:</strong> ${vehicle.inv_color}</p>
-        </div>
-      </div>
-    `;
-  }
-  
-  module.exports = {
-    // ... other exports
-    buildVehicleDetailView,
-  };
+// database/index.js
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Important for Render-hosted databases
+  },
+});
+
+module.exports = pool;
+
   
