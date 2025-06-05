69243ad1afcacbc004df61e6df96df5f6f191245
jimmy opot alando <jimmyopot18@gmail.com>
2025-06-05 09:24:23 +0300

    W04 Assignment: Adding New Classifications and Vehicles

--- routes/invRoute.js
+++ routes/invRoute.js
@@ -1,51 +1,33 @@
-const invValidation = require("../middleware/invValidation")
-
 const express = require("express");
 const router = express.Router();
+
 const invController = require("../controllers/invController");
+const invValidation = require("../middleware/invValidation");
 
 // Route to view individual vehicle details
 router.get("/detail/:invId", invController.buildByInvId);
 
-module.exports = router;
-
-
-
 // Route for the inventory management view
-router.get("/", invController.buildManagementView)
+router.get("/", invController.buildManagementView);
 
-module.exports = router
-
-
-
-
-
-const router = new express.Router()
-const invController = require("../controllers/invController")
-const invValidation = require("../middleware/invValidation")
-
-router.get("/", invController.buildManagementView)
-
-router.get("/add-classification", invController.showAddClassification)
+// Routes for adding classification
+router.get("/add-classification", invController.showAddClassification);
 
 router.post(
   "/add-classification",
   invValidation.classificationRules(),
   invValidation.checkClassificationData,
   invController.processAddClassification
-)
+);
 
-module.exports = router
-
-
-
-const invValidation = require("../middleware/invValidation")
-
-router.get("/add-inventory", invController.buildAddInventory)
+// Routes for adding inventory
+router.get("/add-inventory", invController.buildAddInventory);
 
 router.post(
   "/add-inventory",
   invValidation.inventoryRules(),
   invValidation.checkInventoryData,
   invController.processAddInventory
-)
+);
+
+module.exports = router;
